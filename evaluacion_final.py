10423c8b6dd9e70a076b01a71af8bee4a3f6c01b
Diego Alejandro Lopez Oliveros <54952890+Alejo29@users.noreply.github.com>
2020-01-20 17:28:23 -0500

    Update evaluacion_final.py

--- evaluacion_final.py
+++ evaluacion_final.py
@@ -1,3 +1,34 @@
+"""te proponemos como proyecto final del curso Fundamentos de Programación Python que desarrolles tu propia billetera digital de tipo Desktop con interfaz de texto, que soporte monedas registradas en coinmarketcap.com, y que permita:
+
+    Enviar un monto en USD de alguna de las criptomonedas a un destinatario indicado (identificado por un código)
+    Recibir de un enviador (identificado por un código) una cantidad de alguna criptomoneda
+    Consultar el balance de cada una de las criptomonedas en USD
+    Consultar el balance general del usuario en USD usando el precio de la criptomoneda provisto por las APIs de coinmarketcap.com
+    Emitir un histórico de transacciones del usuario indicando fecha, moneda, cantidad y monto en USD para el momento de la transacción
+    Todas las transacciones realizadas por el usuario deben ser almacenadas y mantenidas, así como las cantidades de cada una de las criptomonedas que posea
+
+Colocar un menú de opciones con:
+
+    Recibir cantidad:
+        Solicitar moneda, cantidad a recibir, así como el código.
+        Validar moneda, cantidad y código, éste debe ser diferente al propio.
+        Sumar cantidad de monedas al saldo.
+    Transferir monto:
+        Solicitar moneda, monto y código del destinatario a enviar.
+        Validar.
+        Restar cantidad de monedas al saldo.
+    Mostrar balance una moneda:
+        Solicitar la moneda a mostrar
+        Validar existencia de la moneda.
+        Mostrar nombre de la moneda, cantidad y monto en USD para ese momento.
+    Mostrar balance general:
+        Mostrar nombre de cada moneda, cantidad y monto en USD para ese momento.
+        Mostrar monto total en USD de todas las monedas.
+    Mostrar histórico de transacciones:
+        Mostrar todas las transacciones indicando fecha, moneda, tipo de operación, código del usuario, cantidad y monto para el momento.
+    Salir del programa
+
+Recuerda hacer las validaciones de las monedas, de los montos, del saldo y de los códigos."""
 from datetime import datetime
 import requests
 #Billetera digital
